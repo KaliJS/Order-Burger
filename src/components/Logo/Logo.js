7689739c84aa1605d5e8bdd948cebfaa43d8c434
Khalid Ali <khalidist759@gmail.com>
2020-04-19 12:51:22 +0530

    after validation

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import burgerLogo from '../../assets/images/burger-logo.png';
+import './Logo.css';
+
+const logo = (props) => (
+    
+        <img src={burgerLogo} alt="MyBurger" />
+    
+);
+
+export default logo;
